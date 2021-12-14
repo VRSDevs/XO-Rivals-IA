3d1ef793d2161454adca1a84908d47072b1f62fa
kalomano <sowerpablo@gmail.com>
2021-12-14 15:32:17 +0100

    state follow huella

--- XORivalsIA/Assets/Scripts/Fantasmas/FSMGhost/StateFollowHuella.cs
+++ XORivalsIA/Assets/Scripts/Fantasmas/FSMGhost/StateFollowHuella.cs
@@ -1,18 +1,10 @@
+using RobustFSM.Base;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StateFollowHuella : MonoBehaviour
+public class StateFollowHuella : BState
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
