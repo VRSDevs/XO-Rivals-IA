4785536382494fefd986518dedc0e7d0e6fe46cb
kalomano <sowerpablo@gmail.com>
2021-12-14 02:33:43 +0100

    llamamos a dejar de asustar
    despues de un tiempo se deja de asustar

--- XORivalsIA/Assets/Scripts/Fantasmas/BTGhost/CheckHuir.cs
+++ XORivalsIA/Assets/Scripts/Fantasmas/BTGhost/CheckHuir.cs
@@ -23,8 +23,10 @@ public class CheckHuir : Node
         float distLuciernaga = Vector3.Distance(_luciernaga.transform.position, _tree.transform.position);
 
 
-        if (distLuciernaga < _tree.distanciaMiedo) //DETECTA LA LINTERNA AL JUGADOR
+        if ((distLuciernaga < _tree.distanciaMiedo) || _tree.asustado  ) //ESTAMOS YA ASUSTADOS O NOS EMPEZAMOS A ASUSTAR
         {
+            _tree.asustado = true;
+            _tree.dejarMiedoEnTiempo();
             state = NodeState.SUCCESS;
             return state;
         }
