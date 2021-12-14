e7f9806c63da8423c6736ba389aa63ee5b3fe8c4
kalomano <sowerpablo@gmail.com>
2021-12-14 01:50:37 +0100

    CheckHuir
    de EnemyBt

--- XORivalsIA/Assets/Scripts/Fantasmas/BTGhost/CheckHuir.cs
+++ XORivalsIA/Assets/Scripts/Fantasmas/BTGhost/CheckHuir.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using BehaviorTree;
 
-public class CheckHuir : MonoBehaviour
+public class CheckHuir : Node
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private GameObject _luciernaga;
+    private EnemyBT _tree;
+
+    public CheckHuir(GameObject luciernaga, EnemyBT tree)
     {
-        
+        _luciernaga = luciernaga;
+        _tree = tree;
+
     }
 
-    // Update is called once per frame
-    void Update()
+
+
+    public override NodeState Evaluate()
     {
-        
+        float distLuciernaga = Vector3.Distance(_luciernaga.transform.position, _tree.transform.position);
+
+
+        if (distLuciernaga < _tree.distanciaMiedo) //DETECTA LA LINTERNA AL JUGADOR
+        {
+            state = NodeState.SUCCESS;
+            return state;
+        }
+        else //NO HUYE
+        {
+            state = NodeState.FAILURE;
+            return state;
+        }
+
+
     }
+
 }
